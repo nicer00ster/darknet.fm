ebe14b6159be07c5d7ceb640edacb44f0ef99c60
Alex Busch <abusch1013@gmail.com>
2019-03-31 12:53:03 -0600

    deployed :D

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -1,4 +1,4 @@
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://darknetfm-next-prod.herokuapp.com/library`;
+export const prodEndpoint = `https://darknet-yoga-prod.herokuapp.com/`;
 export const perPage = 8;
 export const userPerPage = 3;
